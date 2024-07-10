15df753f7031744c7d0af98eabcb1bf7e1cdcf5b
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-10 13:13:31 +0300

    modified  index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Sam'
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a new object with all existing key-value pairs and add/update the new key-value pair
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Modify the existing employee object with the new key-value pair
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object by omitting the specified key from the employee object
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Modify the existing employee object by deleting the specified key
+    delete employee[key];
+    return employee;
+  }
+
+  
